9b2f63efdfffbcd80aae10271d2455799d44cf6f
Jaylano <jaylanovanderveen@gmail.com>
2025-09-13 23:14:23 +0200

    Refactor user registration and login functions to enhance email existence check and improve login response structure

--- register/register.php
+++ register/register.php
@@ -1,10 +1,36 @@
 <?php
+
+function checkIfEmailExists($email, $conn)
+{
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+    return mysqli_num_rows($result) > 0;
+}
+
 function addUser($data, $conn)
 {
     $firstName = $data['firstName'] ?? '';
     $lastName = $data['lastName'] ?? '';
     $email = $data['email'] ?? '';
     $password = $data['password'] ?? '';
+
+    if (checkIfEmailExists($email, $conn)) {
+        echo json_encode([
+            "success" => false,
+            "message" => "Dit email adress is al geregistreerd, probeer een andere"
+        ]);
+        return;
+    }
+
+    if (empty($firstName) || empty($lastName) || empty($email) || empty($password)) {
+        echo json_encode([
+            "success" => false,
+            "message" => "Alle velden zijn verplicht"
+        ]);
+        return;
+    }
+
+
 
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
@@ -28,16 +54,44 @@ function checkLogin($data, $conn)
     $result = mysqli_query($conn, $sql);
     $user = mysqli_fetch_assoc(result: $result);
 
+    $loggedInData = getLoginData($data, $conn);
+
     if ($user && password_verify($password, $user['password'])) {
         echo json_encode([
             "success" => true,
             "message" => "Login successful",
-            "userId" => $user['id']
+            "userId" => $user['id'],
+            "LoggedInData" => $loggedInData
         ]);
     } else {
         echo json_encode([
             "success" => false,
-            "message" => "Invalid email or password"
+            "message" => "Email of wachtwoord is onjuist"
+        ]);
+    }
+}
+
+
+function getLoginData($data, $conn)
+{
+    $email = $data['email'] ?? '';
+
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc(result: $result);
+
+    if ($user) {
+        echo json_encode([
+            "success" => true,
+            "userId" => $user['id'],
+            "firstName" => $user['firstname'],
+            "lastName" => $user['lastname'],
+            "email" => $user['email']
+        ]);
+    } else {
+        echo json_encode([
+            "success" => false,
+            "message" => "User not found"
         ]);
     }
 }
